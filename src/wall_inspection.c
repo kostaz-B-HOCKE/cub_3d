a1a7a069d33fe3af2e9239b5d906b92d56c7b4e2
Emma Abradolf <eabradol@at-f4.msk.21-school.ru>
2022-06-15 15:00:24 +0300

    work_map

--- src/wall_inspection.c
+++ src/wall_inspection.c
@@ -135,37 +135,51 @@ int look_top_right(t_info *o)
 
 int look_low_right(t_info *o)
 {
-    if (o->argv[o->y + 1][o->x] && o->argv[o->y + 1][o->x] == '1' && o->check[o->y + 1][o->x] != 'W')
+    int flag;
+
+    flag = 0;
+    if (o->argv[o->y + 1][o->x - 1] && o->argv[o->y + 1][o->x - 1] == '1' && o->check[o->y + 1][o->x - 1] != 'W')
+        flag = 1;
+    if (o->argv[o->y + 1][o->x] && o->argv[o->y + 1][o->x] == '1' && o->check[o->y + 1][o->x] != 'W' && flag != 1)
     {
         o->y++;
         o->check[o->y][o->x] = 'W';
         return (1);
     }
-    if (o->argv[o->y + 1][o->x + 1] && o->argv[o->y + 1][o->x + 1] == '1' && o->check[o->y + 1][o->x + 1] != 'W')
+    if (o->argv[o->y + 1][o->x + 1] && o->argv[o->y + 1][o->x + 1] == '1' && o->check[o->y + 1][o->x + 1] != 'W' && flag != 1)
     {
         o->y++;
         o->x++;
         o->check[o->y][o->x] = 'W';
         return (1);
     }
+    if (flag == 1)
+        return (2);
     return (0);
 }
 
 int look_low_left(t_info *o)
 {
-    if (o->argv[o->y][o->x - 1] && o->argv[o->y][o->x - 1] == '1' && o->check[o->y][o->x - 1] != 'W')
+    int flag;
+
+    flag = 0;
+    if (o->argv[o->y - 1][o->x -1] && o->argv[o->y - 1][o->x - 1] == '1' && o->check[o->y - 1][o->x -1] != 'W')
+        flag = 1;
+    if (o->argv[o->y][o->x - 1] && o->argv[o->y][o->x - 1] == '1' && o->check[o->y][o->x - 1] != 'W' && flag != 1)
     {
         o->x--;
         o->check[o->y][o->x] = 'W';
         return (1);
     }
-    else if (o->argv[o->y + 1][o->x - 1] && o->argv[o->y + 1][o->x - 1] == '1' && o->check[o->y + 1][o->x - 1] != 'W')
+    else if (o->argv[o->y + 1][o->x - 1] && o->argv[o->y + 1][o->x - 1] == '1' && o->check[o->y + 1][o->x - 1] != 'W' && flag != 1)
     {
         o->x--;
         o->y++;
         o->check[o->y][o->x] = 'W';
         return (1);
     }
+    if (flag == 1)
+        return (2);
     return (0);
 }
 
@@ -173,33 +187,90 @@ int look_top_while(t_info *o)
 {
     int i;
 
-    o->x--;
-    i = look_top_left(o);
+//    o->x--;
+    i = 1;
     while (i == 1)
         i = look_top_left(o);
-//    while (i == 1)
-//        i = look_top_right(o);
-    printf("%d\n", i);
+
+    if (i != 2)
+        return (1);
+    else
+        i = 1;
+    while (i == 1)
+        i = look_top_right(o);
+
+    if (i != 2)
+        return (1);
+    else
+        i = 1;
+    while (i == 1)
+        i = look_low_right(o);
+
+    if (i != 2)
+        return (1);
+    else
+        i = 1;
+    while (i == 1)
+        i = look_low_left(o);
+    if (i != 2)
+        return (1);
+//    else
+//        i = 1
+//    printf("TTT\n");
+//    printf("y:%d\nx:%d\nee:%c\n", o->y, o->x, o->argv[o->y][o->x]);
+//    ft_print_darr(o->check, 1);
+    while (i == 1)
+        i = look_top_left(o);
+    if (i != 2)
+        return (1);
     ft_print_darr(o->check, 1);
     return (0);
 }
 
-//далее название *info сокращено до *o что было более читаемо
+int cheak_coup(t_info *o)
+{
+    int flag;
+
+    flag = 1;
+//    ft_print_darr(o->argv, 1);
+    while (flag == 1)
+    {
+        flag = 0;
+        while (o->argv[o->y][o->x - 1] && o->argv[o->y][o->x] && o->argv[o->y][o->x] != '1')
+            o->x--;
+        if (!look_top_while(o))
+        {
+            printf("YES\n");
+            return (0);
+        }
+        else if (o->argv[o->y][o->x - 1])
+        {
+            o->x--;
+            flag = 1;
+            printf("NO\n");
+        }
+    }
+    return (1);
+}
+
+//далее название *info сокращено до *o что бы стало читаемо
 void    check_wall(t_info *o)
 {
     o->check = ft_strdup_two(o->argv);
-//    ft_print_darr(o->check, 1);
     while (o->argv[++o->y])
     {
         o->x = -1;
         while (o->argv[o->y][++o->x])
             if (o->argv[o->y][o->x] == 'N')
             {
-//                o->check[o->y][o->x] = '9';
+                o->check[o->y][o->x] = '9';
 //                look_left_unit(o);
-                o->check[o->y][o->x] = 'W';
-                look_top_while(o);
-//                look_top_unit(o);
+//                o->check[o->y][o->x] = 'W';
+                cheak_coup(o);
+//                if (!look_top_while(o))
+//                    printf("YES\n");
+//                else
+//                    printf("NO\n");
                 return ;
             }
     }
@@ -212,6 +283,5 @@ int wall_inspection(t_info *info)
 //    printf("'/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/''/'\n");
     cheak_plaer(info);
     check_wall(info);
-
     return (0);
 }
